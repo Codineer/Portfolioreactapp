3667f3cc907b81cab382c689763ec0c27212e9af
Codineer <utkarshgamer294@outlook.com>
2024-02-27 00:18:15 +0530

    commit

---
+++ src/app/components/UpcomingWebinars.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UpcomingWebinars = () => {
+    return (
+        <div className='p-12 bg-gray-900'>
+            <div className='max-w-7xl mx-auto px-4 sm:px-6'>
+                <div className='text-center'>1</div>
+                <div className='m-10'>2</div>
+                <div className='mt-10 text-center'>3</div>
+            </div>
+        </div>
+    )
+}
+
+export default UpcomingWebinars
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import HeroSection from "./components/HeroSection";
 import WhyChooseUs from "./components/WhyChooseUs";
 import { MovingCards } from "./components/MovingCards";
 import FeaturedSection from "./components/FeaturedSection";
+import UpcomingWebinars from "./components/UpcomingWebinars";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] ">
@@ -9,6 +11,7 @@ export default function Home() {
       <FeaturedSection />
       <WhyChooseUs />
       <MovingCards />
+      <UpcomingWebinars />
     </main>
 
   );
